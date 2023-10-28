11fa62e23e1ede0969be836f0296e7d1902499e6
minusyt23 <altirtertyo@gmail.com>
2023-10-28 18:28:33 +0200

    Uploaded to Github.

---
+++ index.md
@@ -0,0 +1,6 @@
+# Gli appunti.
+
++ [Index](index.md)
++ [Leopardi](leopardi.md)
++ [Schopenhauer](schopenhauer.md)
+
---
+++ upload-github.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Build the "index.md" file
+
+index_file="# Gli appunti.\n\n"
+
+for file in $(ls *.md)
+do
+	# Build the file name
+	name=$(basename $file .md)
+	name="$(echo $name | head -c1 | tr [:lower:] [:upper:])$(echo $name | cut -c2-)"
+
+	index_file="$index_file+ [$name]($file)\n"
+done
+
+echo -e $index_file > index.md
+
+# Publish all to github.
+
+git add .
+
+git commit -m "Uploaded to Github."
+
+token=$(cat "github-token")
+
+git push "https://$token@github.com/minusyt23/notes.git main
